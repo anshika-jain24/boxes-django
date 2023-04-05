59cc402e876c3f5d707fc5d04f867ad0ce113872
Anshika Jain <jainanshika193@gmail.com>
2023-04-05 11:59:08 +0530

    deploy1

--- spinny/settings.py
+++ spinny/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-fmfmn#(f@7!gj+r7&cp8c=oz$mx84ya2_xhyq1wf&8aif(%2a7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
