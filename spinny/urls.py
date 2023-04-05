ea1257e02bb12ba674dc2596362858ca63730969
Anshika Jain <jainanshika193@gmail.com>
2023-04-05 12:07:52 +0530

    deploy2

--- spinny/urls.py
+++ spinny/urls.py
@@ -18,7 +18,7 @@ from django.urls import include, path
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
-    path('boxes/', include('boxes.urls')),
+    path('', include('boxes.urls')),
     path('admin/', admin.site.urls),
 ]
 
